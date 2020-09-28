cc868d8f8cfc707ceffd8a3ba74a01b0a8df1b2e
Ryan Zigler <rpzigler@gmail.com>
2020-09-28 14:25:20 -0500

    update for public

--- eslint-prettier-config.sh
+++ eslint-prettier-config.sh
@@ -173,22 +173,22 @@ echo -e "${GREEN}Finished setting up!${NC}"
 echo
 
 
-{
-  "arrowParens": "always",
-  "bracketSpacing": true,
-  "embeddedLanguageFormatting": "auto",
-  "htmlWhitespaceSensitivity": "css",
-  "insertPragma": false,
-  "jsxBracketSameLine": false,
-  "jsxSingleQuote": true,
-  "printWidth": 80,
-  "proseWrap": "preserve",
-  "quoteProps": "as-needed",
-  "requirePragma": false,
-  "semi": true,
-  "singleQuote": true,
-  "tabWidth": 2,
-  "trailingComma": "es5",
-  "useTabs": false,
-  "vueIndentScriptAndStyle": false
-}
+# {
+#   "arrowParens": "always",
+#   "bracketSpacing": true,
+#   "embeddedLanguageFormatting": "auto",
+#   "htmlWhitespaceSensitivity": "css",
+#   "insertPragma": false,
+#   "jsxBracketSameLine": false,
+#   "jsxSingleQuote": true,
+#   "printWidth": 80,
+#   "proseWrap": "preserve",
+#   "quoteProps": "as-needed",
+#   "requirePragma": false,
+#   "semi": true,
+#   "singleQuote": true,
+#   "tabWidth": 2,
+#   "trailingComma": "es5",
+#   "useTabs": false,
+#   "vueIndentScriptAndStyle": false
+# }
